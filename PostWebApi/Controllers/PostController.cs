6b749435561a1c4d29970571b854f08204d7ab7f
Aditya <abeysingheaditya4@gmail.com>
2026-08-27 18:03:18 +0530

    Live endpoint and nuget packages

---
+++ PostWebApi/Controllers/PostController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PostWebApi.Controllers
+{
+    [Route("api/v1/post/")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        [HttpGet("live")]
+        public IActionResult Live()
+        {
+            return Ok("Post API is live");
+        }
+    }
+}
