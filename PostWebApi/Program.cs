22d665d8560d002da03b5ba1d74979dc0032ad3d
Aditya <abeysingheaditya4@gmail.com>
2026-08-29 20:01:00 +0530

    Fixed Swagger issue

--- PostWebApi/Program.cs
+++ PostWebApi/Program.cs
@@ -1,4 +1,3 @@
-
 namespace PostWebApi
 {
     public class Program
@@ -7,25 +6,25 @@ namespace PostWebApi
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.MapOpenApi(); 
+                app.UseStaticFiles();
+
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/openapi/v1.json", "PostWebApi v1");
+                    options.RoutePrefix = "swagger";
+                });
             }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
 
             app.Run();
