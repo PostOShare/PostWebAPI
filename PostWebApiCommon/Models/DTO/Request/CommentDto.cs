b27f5243f4b5dfca0d4ceea97beb0833b5e1420f
Aditya <abeysingheaditya4@gmail.com>
2026-08-30 22:59:40 +0530

    PostWebApiCommon project and Post models

---
+++ PostWebApiCommon/Models/DTO/Request/CommentDto.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace PostWebApiCommon.Models.DTO.Request
+{
+    public class CommentDto
+    {
+        [BsonElement("commentId")]
+        public string CommentId { get; set; }
+
+        [BsonElement("userId")]
+        public string UserId { get; set; }
+
+        [BsonElement("text")]
+        public string Text { get; set; }
+
+        [BsonElement("createdAt")]
+        public string CreatedAt { get; set; }
+
+        [BsonElement("lastUpdatedAt")]
+        public string LastUpdatedAt { get; set; }
+
+        [BsonElement("subComments")]
+        public List<SubCommentDto> SubComments { get; set; } = new();
+    }
+}
---
+++ PostWebApiCommon/Models/DTO/Request/MediaDto.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace PostWebApiCommon.Models.DTO.Request
+{
+    public class MediaDto
+    {
+        [BsonElement("type")]
+        public string Type { get; set; }
+
+        [BsonElement("url")]
+        public string Url { get; set; }
+
+        [BsonElement("createdAt")]
+        public string CreatedAt { get; set; }
+
+        [BsonElement("visibility")]
+        public string Visibility { get; set; }
+    }
+}
